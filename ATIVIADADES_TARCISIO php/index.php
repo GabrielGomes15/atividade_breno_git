f0d64826791d70af1b48df9dc2b2e2d1b9be1517
Gabriel <gabriel.ferraz@aluno.ifsertao-pe.edu.br>
2026-07-10 09:29:21 -0300

    a

--- ATIVIADADES_TARCISIO php/index.php
+++ ATIVIADADES_TARCISIO php/index.php
@@ -1,2 +1,18 @@
-<?php 
-  $nome = $_POST["nome"];
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form action="processa.php" method="post">
+<label>Nome:</label>
+<input type="text" name="nome">
+<br><br>
+<button type="submit">
+Enviar
+</button>
+</form>
+</body>
+</html>
---
+++ ATIVIADADES_TARCISIO php/processa.php
@@ -0,0 +1,17 @@
+<?php
+$nome = $_POST['nome'];
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Resultado</title>
+</head>
+<body>
+<h1>Dados Recebidos</h1>
+<p>
+Bem-vindo(a),
+<?php echo $nome; ?>!
+</p>
+</body>
+</html>
